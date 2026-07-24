d8f1b7d53d7669dbc0a48136012dcf4f01c831ed
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-25 00:32:03 +0530

    get data by id

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def greet():
 
 products = [
     Product(id = 1,name = "Phone",description = "Apple Phone",price = 1000,quantity= 5),
-    Product(id = 1,name = "Phone",description = "Apple Phone",price = 1000,quantity= 5),
-    Product(id = 1,name = "Phone",description = "Apple Phone",price = 1000,quantity= 5),  
+    Product(id = 2,name = "Lap",description = "Apple Phone",price = 1000,quantity= 5),
+    Product(id = 3,name = "Tab",description = "Apple Phone",price = 1000,quantity= 5),  
 ]
+
 @app.get("/products")
 def getAllProducts():
     return products
+
+@app.get("/product/{id}")
+def get_product_by_id(id: int):
+    return products[id-1]
