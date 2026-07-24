6c10852ad57e3aec49b6380370a5844e735cd8af
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-25 01:31:13 +0530

    added basic full CURD operation

--- main.py
+++ main.py
@@ -10,14 +10,35 @@ products = [
     Product(id = 2,name = "Lap",description = "Apple Phone",price = 1000,quantity= 5),
     Product(id = 3,name = "Tab",description = "Apple Phone",price = 1000,quantity= 5),  
 ]
-
+#GET
 @app.get("/products")
 def getAllProducts():
     return products
-
+#GET BY ID
 @app.get("/product/{id}")
 def get_product_by_id(id: int):
     for product in products:
         if product.id == id:
             return product
     return("Product Not Found")
+#ADD
+@app.post("/product")
+def add_products(product:Product):
+    products.append(product)
+    return(product)
+#UPDATE
+@app.put("/product")
+def update_product(id:int, product: Product):
+    for i in range(len(products)):
+        if products[i].id == id:
+            products[i] = product
+            return "Product added successfully"
+    return "No product found"
+#DELETE
+@app.delete("/product")
+def delete_product(id:int):
+    for i in range(len(products)):
+        if products[i].id == id:
+            del products[i]
+            return "Product delete successfully"
+    return "No product found"
