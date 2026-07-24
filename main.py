0a281e66d3e38cf2c1086c65e6f7c3f0f11e38a9
Sachin Mudannayaka <sachiemudannayaka@gmail.com>
2026-07-25 00:07:31 +0530

    Define a model and pass dictionary to return

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
-
+from models import Product
 app = FastAPI()
 @app.get("/")
 def greet():
     return("Hello world")
+
+products = [
+    Product(1,"Phone","Apple Phone",1000,5),
+    Product(2,"Lap","Dell Lap",200,50),
+    Product(3,"Tab","KIA Phone",20.89,15)
+]
+@app.get("/products")
+def getAllProducts():
+    return products
